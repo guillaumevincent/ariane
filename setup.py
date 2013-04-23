37ccd37e06c1f59878872f5b576b72c464226aca
Guillaume Vincent <gvincent@oslab.fr>
2013-04-23 12:41:12 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ from distutils.core import setup
 
 setup(
     name='Ariane',
-    version='0.0.1',
+    version='0.0.2',
     author='Guillaume Vincent',
     author_email='vincent.guillaume.inp@gmail.com',
     packages=['ariane', 'ariane.test'],
     url='https://github.com/guillaumevincent/Ariane',
-    license='LICENSE',
+    license='WTFPL - DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE',
     description='Ariane is a python package for describing, producing and visualizing RESTful APIs',
     long_description=open('README').read(),
-    install_requires=[
-        "Jinja2 >= 0.7.6",
-    ],
 )
