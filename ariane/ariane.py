15869c376041bfa439c012b216b29c37de857409
Guillaume Vincent <gvincent@oslab.fr>
2013-05-24 20:10:30 +0200

    handle unicode config parser file

--- ariane/ariane.py
+++ ariane/ariane.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+import codecs
+import os
+from jinja2 import Environment, FileSystemLoader
+
 __author__ = 'gvincent'
 import ConfigParser
 
@@ -7,28 +12,46 @@ class Documentation(object):
     def __init__(self, documentation_file):
         try:
             with open(documentation_file):
+                self.resources = []
                 self.parse(documentation_file)
                 self.is_valid = True
+                self.error_message = ''
         except IOError:
             self.status_code = 400
             self.is_valid = False
             self.error_message = "%s file doesn't exist!" % documentation_file
 
     def parse(self, documentation_file):
-        config = ConfigParser.RawConfigParser()
-        config.read(documentation_file)
+        """Parse documentation_file and add dynamically resources and section in self object attribute
+        """
+        config = ConfigParser.ConfigParser()
+        config.readfp(codecs.open(documentation_file, "r", "utf8"))
+
         for section in config.sections():
             options = {}
             for option in config.options(section):
                 options[option] = config.get(section, option)
 
             if ":" in section:
-                resource = section.split(':')[0]
-                methods = self.__dict__[resource] if resource in self.__dict__ else []
                 options["method"] = section.split(':')[1]
-                methods.append(options)
-                self.__dict__[resource] = methods
+                options["name"] = section.split(':')[0]
+                self.resources.append(options)
             else:
                 self.__dict__[section] = options
 
-
+    def generate(self, template, output_file):
+        """Generate documentation with jinja2
+        """
+        template = os.path.normpath(template)
+        try:
+            with open(template):
+                env = Environment()
+                env.loader = FileSystemLoader(os.path.dirname(template))
+                template = env.get_template(os.path.basename(template))
+                rendering = template.render(doc=self.__dict__)
+                with open(os.path.normpath(output_file), 'w') as doc:
+                    doc.write(rendering.encode('utf-8'))
+        except IOError:
+            self.status_code = 400
+            self.is_valid = False
+            self.error_message = "%s template file doesn't exist!" % template
--- ariane/tests/ariane_tests.py
+++ ariane/tests/ariane_tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import json
 
 import os
 import unittest
@@ -11,13 +12,21 @@ __author__ = 'gvincent'
 
 class TestsAriane(unittest.TestCase):
 
+    def setUp(self):
+        self.documentation_file = os.path.join(os.path.dirname(__file__), 'ariane_documentation.ini')
+        self.ariane_documentation = ariane.Documentation(self.documentation_file)
+
     def test_Ariane_object_need_an_existing_documentation_file(self):
         documentation_file = os.path.join(os.path.dirname(__file__), 'devil.json')
         ariane_documentation = ariane.Documentation(documentation_file)
         self.assertEqual(400, ariane_documentation.status_code)
         self.assertTrue("devil.json file doesn't exist!" in ariane_documentation.error_message)
 
-    def test_Ariane_object_has_a_section_in_attributes(self):
+    def test_relative_documentation_file(self):
+        ariane_documentation = ariane.Documentation("ariane_documentation.ini")
+        self.assertTrue(ariane_documentation.is_valid)
+
+    def test_Ariane_with_a_random_section(self):
         random_name = "".join([random.choice(string.letters) for i in xrange(15)])
         documentation_file = os.path.join(os.path.dirname(__file__), random_name)
         with open(documentation_file, 'w') as f:
@@ -26,29 +35,36 @@ class TestsAriane(unittest.TestCase):
         os.remove(documentation_file)
         self.assertTrue(hasattr(ariane_documentation, random_name))
 
-    def setUp(self):
-        self.documentation_file = os.path.join(os.path.dirname(__file__), 'ariane_documentation.ini')
-        self.ariane_documentation = ariane.Documentation(self.documentation_file)
-
     def test_Ariane_attributes(self):
         self.assertEqual('1.0.0', self.ariane_documentation.api["version"])
 
     def test_Ariane_unique_resource(self):
         dog = {
+            "name": "dog",
             "method": "GET",
             "url": "dog/"
         }
-        self.assertEqual(dog, self.ariane_documentation.dog[0])
+        self.assertEqual(dog, self.ariane_documentation.resources[0])
 
-    def test_Ariane_resources_with_multiple_html_verbs(self):
-        self.assertEqual(2, len(self.ariane_documentation.dog))
+    def test_Ariane_test_api_doc_has_2_resources(self):
+        self.assertEqual(2, len(self.ariane_documentation.resources))
 
     def test_Ariane_multiple_input_lines(self):
         expected_input_post_dog_request = '\n{\n"username": "",\n"password": ""\n}'
-        self.assertEqual(expected_input_post_dog_request, self.ariane_documentation.dog[1]['input'])
+        self.assertEqual(expected_input_post_dog_request, self.ariane_documentation.resources[1]['input'])
 
-    def test_Ariane_unicode_verification(self):
-        self.assertEqual('嶺', self.ariane_documentation.dog[1]['name'])
+    def test_Ariane_unicode_resource(self):
+        self.assertEqual('嶺'.decode('utf-8'), self.ariane_documentation.resources[1]['info'])
+
+
+class TestPrintDocumentation(unittest.TestCase):
+
+    def test_print(self):
+        doc = ariane.Documentation('ariane_documentation.ini')
+        template = os.path.join(r'', os.path.dirname(os.path.dirname(__file__)), 'documentation', 'template.html')
+        output_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'documentation', 'demo.html')
+        doc.generate(template, output_file)
+        self.assertTrue(doc.is_valid)
 
 if __name__ == '__main__':
     unittest.main()
